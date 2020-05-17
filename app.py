d5aa47bfe2bcefcca1cb891b6740d6cdc29fee14
Nicolaz Perez <nicolaz888@hotmail.com>
2020-05-16 19:14:07 -0500

    propagate exceptions added

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from security import authenticate, identity
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = 'True'
 app.secret_key = 'jose'
 api = Api(app)
 
--- resources/store.py
+++ resources/store.py
@@ -38,5 +38,6 @@ class Store(Resource):
 
 class StoreList(Resource):
 
+    @jwt_required()
     def get(self):
         return {'stores': [store.json() for store in StoreModel.query.all()]}
